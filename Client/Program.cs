3d7bd77bb2d9887d4f141d396b0aed4b40b1045e
Luis Franco <sterlux@gmail.com>
2024-02-12 02:05:51 -0800

    Init

---
+++ Client/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var ip = IPAddress.Loopback;
+var port = 13000;
+
+var remoteEP = new IPEndPoint(ip, port);
+
+using (var socket = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp))
+{
+    socket.Connect(remoteEP);
+    Console.WriteLine($"Socket connected to {socket.RemoteEndPoint.ToString()}");
+
+    var msg = Encoding.ASCII.GetBytes("This is a test");
+    int bytesSent = socket.Send(msg);
+
+    var bytes = new byte[1024];
+    int bytesRec = socket.Receive(bytes);
+    Console.WriteLine($"Echoed test = {Encoding.ASCII.GetString(bytes, 0, bytesRec)}");
+
+    Thread.Sleep(100000);
+    
+    socket.Shutdown(SocketShutdown.Both);
+    socket.Close();
+}
---
+++ Server/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+var ip = IPAddress.Loopback;
+var port = 13000;
+var localEndPoint = new IPEndPoint(ip, port);
+
+using (var socket = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp))
+{
+    socket.Bind(localEndPoint);
+    socket.Listen(10);
+
+    Console.WriteLine($"Server listening on {ip}:{port}");
+    var handler = socket.Accept();
+
+    var bytes = new byte[1024];
+    var bytesRec = handler.Receive(bytes);
+    var data = Encoding.ASCII.GetString(bytes, 0, bytesRec);
+    Console.WriteLine($"Received: {data}");
+
+    var msg = Encoding.ASCII.GetBytes("Message received.");
+    handler.Send(msg);
+    Thread.Sleep(100000);
+
+    handler.Shutdown(SocketShutdown.Both);
+    handler.Close();
+}
